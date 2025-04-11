814e603efacf54fb153169397156d9c35c345b1a
sailajasuprava <sailaja.suprava@gmail.com>
2025-04-11 09:46:14 +0530

    ui updated

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -25,8 +25,8 @@ function Category() {
   if (!products.length) return <Spinner />;
 
   return (
-    <div className="max-w-5xl mx-auto space-y-10">
-      <ul className="grid grid-cols-3 gap-10">
+    <div className="max-w-5xl mx-auto space-y-10 px-4">
+      <ul className="grid xs:grid-cols-2 md:grid-cols-3 2xl:grid-cols-4 gap-10">
         {products.map((prod) => (
           <ProductCard key={prod.id} prod={prod} />
         ))}
